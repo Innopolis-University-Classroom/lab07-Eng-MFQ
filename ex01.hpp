7a7f02a968ca013a309685c107037fec94710ffd
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-06 15:55:48 +0300

    exe 1

--- ex01.hpp
+++ ex01.hpp
@@ -1,8 +1,19 @@
-
+#include <iostream>
+#include <vector>
+using namespace std;
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
+template <class T>
+void multiples (T sum, T x, int n)
 {
-
+    int i;
+    auto term = (T) 0;
+    sum = (T) 1;
+    for (i=1; i<=n; i++)
+    {
+        term = term + x;
+        sum = sum + term;
+    }
+    cout << sum << endl;
 }
