f9e093c172702093a51e651ca52875ba7055e705
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-06 16:04:25 +0300

    exe 1

--- ex02.hpp
+++ ex02.hpp
@@ -12,10 +12,10 @@ public:
         num2 = n2;
     }
 
-//    T add() { return  num1+num2; }
-//    T subtract() { return num1-num2; }
-//    T multiply() { return num1*num2; }
-//    T divide() { return num1/num2; }
+    T add() { return  num1+num2; }
+    T subtract() { return num1-num2; }
+    T multiply() { return num1*num2; }
+    T divide() { return num1/num2; }
 };
 
 
