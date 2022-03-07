da0c8209a3324f037438c05f9ec4a96b37e2a546
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-07 16:31:34 +0300

    exe 1

--- ex01.hpp
+++ ex01.hpp
@@ -4,16 +4,8 @@ using namespace std;
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <class T>
-void multiples (T& sum, T x, int n)
+
+void multiples (int& sum, int x, int n)
 {
-    int i;
-    auto term = (T) 0;
-    sum = (T) 1;
-    for (i=1; i<=n; i++)
-    {
-        term = term + x;
-        sum = sum + term;
-    }
-    cout << sum << endl;
+
 }
