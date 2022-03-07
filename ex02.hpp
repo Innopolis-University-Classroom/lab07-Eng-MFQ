e8eca6673ae67324282e989c56dd1e3359333b34
Muwaffaq Imam <muwaffaqimam@gmail.com>
2022-03-07 15:03:37 +0300

    exe 1

--- ex02.hpp
+++ ex02.hpp
@@ -3,19 +3,7 @@
 // also fill out the body
 template<class T>
 class Calculator {
-private:
-    T num1, num2;
 
-public:
-    Calculator(T n1, T n2) {
-        num1 = n1;
-        num2 = n2;
-    }
-
-    T add() { return  num1+num2; }
-    T subtract() { return num1-num2; }
-    T multiply() { return num1*num2; }
-    T divide() { return num1/num2; }
 };
 
 
